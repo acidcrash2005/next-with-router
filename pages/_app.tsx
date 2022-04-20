5bd5e9044d8e5c037076feeeb85d7926f6b3af43
acidcrash <acidcrash@syneforge.com>
2022-04-20 11:44:55 +0300

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useRouter} from "next/router";
+import {StaticRouter} from "react-router-dom/server";
+import {BrowserRouter} from "react-router-dom";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }: AppProps) {
+    const { query } = useRouter();
+
+    console.log(pageProps,router);
+
+    return typeof window === 'undefined' ? (
+        <StaticRouter location={`/${query.page}`}>
+            <Component {...pageProps} />
+        </StaticRouter>
+    ):<BrowserRouter>
+        <Component {...pageProps} />
+    </BrowserRouter>
 }
 
 export default MyApp
